4d6fb624299817c35bcf4e365dda6fc0a9f44dc6
Jose Manuel <you@example.com>
2020-03-16 11:44:19 -0500

    Url to test added

--- src/main/java/co/com/choucair/certification/github/util/Constants.java
+++ src/main/java/co/com/choucair/certification/github/util/Constants.java
@@ -1,7 +1,7 @@
 package co.com.choucair.certification.github.util;
 
 public class Constants {
-    public static final String URL = "https://github.com/";
+    public static final String URL = "https://angular-vkwfzw.stackblitz.io";
     public static final int INDEX_0 = 0;
     public static final int INDEX_1 = 1;
     private Constants(){
