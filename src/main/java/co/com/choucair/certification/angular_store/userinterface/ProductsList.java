ebb639d24e39d2a71cdb2bac96a3b7d629b59458
Jose Manuel <jose.echeverri@upb.edu.co>
2020-03-20 11:05:21 -0500

    Edited mapping target

--- src/main/java/co/com/choucair/certification/angular_store/userinterface/ProductsList.java
+++ src/main/java/co/com/choucair/certification/angular_store/userinterface/ProductsList.java
@@ -4,8 +4,8 @@ import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class ProductsList {
-//    public static final Target
+
     public static Target getTargetByid(String id){
-        return Target.the("").located(By.id("z"+id));
+        return Target.the("").located(By.id(id));
     }
 }
