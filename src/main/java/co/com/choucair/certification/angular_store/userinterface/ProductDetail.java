4c47aa3a0bb57684b0183d67868f921d4bdb4cf6
Jose <jose.echeverri@upb.edu.co>
2020-03-25 15:15:00 -0500

    Working

--- src/main/java/co/com/choucair/certification/angular_store/userinterface/ProductDetail.java
+++ src/main/java/co/com/choucair/certification/angular_store/userinterface/ProductDetail.java
@@ -9,4 +9,5 @@ public class ProductDetail {
     public static final Target PRODUCT_IVA = Target.the("").located(By.name("iva"));
     public static final Target PRODUCT_TOTAL = Target.the("").located(By.name("total"));
     public static final Target PRODUCT_DESC = Target.the("").located(By.name("description"));
+    public static final Target BUTTON_ADD_TO_CAR = Target.the("").located(By.id("add-car"));
 }
--- src/main/java/co/com/choucair/certification/angular_store/userinterface/ProductsList.java
+++ src/main/java/co/com/choucair/certification/angular_store/userinterface/ProductsList.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class ProductsList {
-
+    public static final Target BUTTON_ADD_TO_CAR = Target.the("").located(By.id("add-car"));
     public static Target getTargetByid(String id) {
         return Target.the(id).located(By.id(id));
     }
